89c7fbed1346b2810e149f7b4e396ec7a482b9f6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-12 11:51:58 +0000

    Again Add PaymentDetails Model.

---
+++ lib/Model/Interfaces/PaymentDetailsInterface.php
@@ -0,0 +1,11 @@
+<?php namespace Vankosoft\PaymentBundle\Model\Interfaces;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface PaymentDetailsInterface extends ResourceInterface
+{
+    public function getUser();
+    public function getPaymentMethod();
+    public function getDetails();
+    public function getType();
+}
