147312cb128405260c8f351180ee92f91dcee245
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-16 07:07:31 +0000

    Improve Payment Model.

--- lib/Model/Interfaces/PaymentInterface.php
+++ lib/Model/Interfaces/PaymentInterface.php
@@ -1,10 +1,10 @@
 <?php namespace Vankosoft\PaymentBundle\Model\Interfaces;
 
 use Sylius\Component\Resource\Model\ResourceInterface;
-use Payum\Core\Model\PaymentInterface;
+use Payum\Core\Model\PaymentInterface as BasePaymentInterface;
 use Payum\Core\Model\DirectDebitPaymentInterface;
 
-interface PaymentInterface extends ResourceInterface, PaymentInterface, DirectDebitPaymentInterface
+interface PaymentInterface extends ResourceInterface, BasePaymentInterface, DirectDebitPaymentInterface
 {
     public function getUser();
     public function getPaidServicePeriod();
