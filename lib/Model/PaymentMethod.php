576305c837123c0f91a308641c0dbbd0a54bff2b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-17 08:19:20 +0000

    Improve PaymentMethod Model.

--- lib/Model/PaymentMethod.php
+++ lib/Model/PaymentMethod.php
@@ -1,20 +1,54 @@
 <?php namespace Vankosoft\PaymentBundle\Model;
 
 use Sylius\Component\Resource\Model\ToggleableTrait;
+use Sylius\Component\Resource\Model\TranslatableTrait;
 
 class PaymentMethod implements Interfaces\PaymentMethodInterface
 {
     use ToggleableTrait;
+    use TranslatableTrait;
     
+    /** @var integer */
     protected $id;
     
-    protected $name;
+    /** @var string */
+    protected $locale;
     
+    /** @var Interfaces\GatewayConfigInterface */
     protected $gateway;
+    
+    /** @var string */
+    protected $slug;
+    
+    /** @var string */
+    protected $name;
     
     public function getId()
     {
         return $this->id;
+    }
+    
+    public function getGateway()
+    {
+        return $this->gateway;
+    }
+    
+    public function setGateway( $gateway )
+    {
+        $this->gateway  = $gateway;
+        
+        return $this;
+    }
+    
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+    
+    public function setSlug($slug=null): self
+    {
+        $this->slug = $slug;
+        return $this;
     }
     
     public function getName()
@@ -25,19 +59,41 @@ class PaymentMethod implements Interfaces\PaymentMethodInterface
     public function setName($name)
     {
         $this->name = $name;
-
         return $this;
     }
     
-    public function getGateway() 
+    public function getActive(): ?bool
     {
-        return $this->gateway;
+        return $this->enabled;
     }
     
-    public function setGateway( $gateway )
+    public function setActive( ?bool $active ): self
     {
-        $this->gateway  = $gateway;
+        $this->enabled = (bool) $active;
+        return $this;
+    }
+    
+    public function isActive()
+    {
+        return $this->isEnabled();
+    }
+    
+    public function getTranslatableLocale(): ?string
+    {
+        return $this->locale;
+    }
+    
+    public function setTranslatableLocale($locale): self
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+    
+    /*
+     * @NOTE: Decalared abstract in TranslatableTrait
+     */
+    protected function createTranslation(): TranslationInterface
+    {
         
-        return $this;
     }
 }
