fea84ee3669db1d3bc2e96d18053c1306bed50ac
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-17 11:04:15 +0000

    Test Payum Prepend Extension

---
+++ lib/DependencyInjection/PrependPayumTrait.php
@@ -0,0 +1,25 @@
+<?php namespace Vankosoft\ApplicationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+trait PrependPayumTrait
+{
+    private function prependPayum( ContainerBuilder $container ): void
+    {
+        if ( ! $container->hasExtension( 'payum' ) ) {
+            return;
+        }
+            
+        if ( $container->hasParameter( 'vankosoft_application.prepend_doctrine_migrations' ) ) {
+            var_dump( $container->getExtensionConfig( 'payum' ) ); die;
+        }
+                
+        $payumConfig = $container->getExtensionConfig( 'payum' );
+        
+        $container->prependExtensionConfig( 'payum', [
+            'migrations_paths' => \array_merge( \array_pop( $doctrineConfig )['migrations_paths'] ?? [], [
+                $this->getMigrationsNamespace() => $this->getMigrationsDirectory(),
+            ]),
+        ]);
+    }
+}
