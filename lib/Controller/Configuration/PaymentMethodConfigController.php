39f6ffcf6f8c8d4a979856479128e5fbe9a6733d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-17 09:35:26 +0000

    Prepair PaymentMethod Resource.

--- lib/Controller/Configuration/PaymentMethodConfigController.php
+++ lib/Controller/Configuration/PaymentMethodConfigController.php
@@ -1,48 +1,21 @@
 <?php namespace Vankosoft\PaymentBundle\Controller\Configuration;
 
+use Vankosoft\ApplicationBundle\Controller\AbstractCrudController;
 use Symfony\Component\HttpFoundation\Request;
-use Payum\Bundle\PayumBundle\Controller\PayumController;
 
-use Vankosoft\PaymentBundle\Form\PaymentMethod as PaymentMethodForm;
-use Vankosoft\PaymentBundle\Entity\PaymentMethod;
-
-class PaymentMethodConfigController extends PayumController
+class PaymentMethodConfigController extends AbstractCrudController
 {
     
-    public function indexAction( Request $request )
+    protected function customData( Request $request, $entity = null ): array
     {
-        $er     = $this->getDoctrine()->getRepository( PaymentMethod::class );
-        
-        return $this->render( 'IAPaymentBundle:PaymentMethodConfig:index.html.twig', [
-            'methods' => $er->findAll()
-        ]);
+        return [
+            
+        ];
     }
     
-    /**
-     * Prepare Action
-     * 
-     * @return type
-     */
-    public function configAction( $id, Request $request )
+    protected function prepareEntity( &$entity, &$form, Request $request )
     {
-        $er     = $this->getDoctrine()->getRepository( PaymentMethod::class );
-        $paymentMethod = $id ? $er->find( $id ) : new PaymentMethod();
         
-        $form = $this->createForm( PaymentMethodForm::class, $paymentMethod );
-     
-        // Form Submit
-        $form->handleRequest( $request );
-        if ( $form->isSubmitted() ) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist( $form->getData() );
-            $em->flush();
-            
-           return $this->redirect( $this->generateUrl( 'ia_payment_methods_index' ) );
-        }
-        
-        return $this->render( 'IAPaymentBundle:PaymentMethodConfig:config.html.twig', [
-            'form'      => $form->createView()
-        ]);
     }
     
 }
---
+++ lib/Controller/Configuration/PaymentMethodConfigExtController.php
@@ -0,0 +1,68 @@
+<?php namespace Vankosoft\PaymentBundle\Controller\Configuration;
+
+use Symfony\Component\HttpFoundation\Request;
+use Payum\Bundle\PayumBundle\Controller\PayumController;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\Component\Resource\Factory\Factory;
+
+use Vankosoft\PaymentBundle\Form\PaymentMethodForm;
+
+class PaymentMethodConfigExtController extends PayumController
+{
+    /** @var string */
+    protected $paymentMethodClass;
+    
+    /** @var EntityRepository */
+    protected EntityRepository $paymentMethodRepository;
+    
+    /** @var Factory */
+    protected Factory $paymentMethodFactory;
+    
+    public function __construct(
+        string $paymentMethodClass,
+        EntityRepository $paymentMethodRepository,
+        Factory $paymentMethodFactory
+    ) {
+        $this->paymentMethodClass       = $paymentMethodClass;
+        $this->paymentMethodRepository  = $paymentMethodRepository;
+        $this->paymentMethodFactory     = $paymentMethodFactory;
+    }
+    
+    public function indexAction( Request $request )
+    {
+        return $this->render( '@VSPayment/Pages/PaymentMethodConfigExt/index.html.twig', [
+            'methods' => $this->paymentMethodRepository->findAll()
+        ]);
+    }
+    
+    /**
+     * Prepare Action
+     * 
+     * @return type
+     */
+    public function configAction( $id, Request $request )
+    {
+        if ( $id ) {
+            $paymentMethod = $this->paymentMethodRepository->find( $id );
+        } else {
+            $paymentMethod = $this->paymentMethodFactory->createNew();
+        }
+        
+        $form = $this->createForm( PaymentMethodForm::class, $paymentMethod );
+     
+        // Form Submit
+        $form->handleRequest( $request );
+        if ( $form->isSubmitted() ) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist( $form->getData() );
+            $em->flush();
+            
+           return $this->redirect( $this->generateUrl( 'vs_payment_methods_index' ) );
+        }
+        
+        return $this->render( '@VSPayment/Pages/PaymentMethodConfigExt/config.html.twig', [
+            'form'      => $form->createView()
+        ]);
+    }
+}
