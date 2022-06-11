ce78adca496c11d1c04336fe0a05bf25c217c844
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-06-11 14:22:25 +0000

    Create Paid Subscription When Payment is Done Successfully.

--- lib/Model/Interfaces/PayableObjectInterface.php
+++ lib/Model/Interfaces/PayableObjectInterface.php
@@ -7,4 +7,7 @@ interface PayableObjectInterface extends ResourceInterface
     public function getPrice();
     public function getCurrencyCode();
     public function getOrderItems();
+    
+    public function getSubscriptionCode(): ?string;
+    public function getSubscriptionPriority(): ?int;
 }
