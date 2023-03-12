2128fe605c6b54bec0d998bdd3f89c20b7c0aefc
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-03-12 20:19:06 +0000

    Add Product  Resources

---
+++ lib/Controller/ProductController.php
@@ -0,0 +1,65 @@
+<?php namespace Vankosoft\PaymentBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Collections\ArrayCollection;
+use Vankosoft\ApplicationBundle\Controller\AbstractCrudController;
+
+class ProductController extends AbstractCrudController
+{
+    protected function customData( Request $request, $entity = null ): array
+    {
+        $translations   = $this->classInfo['action'] == 'indexAction' ? $this->getTranslations() : [];
+        
+        $taxonomy   = $this->get( 'vs_application.repository.taxonomy' )->findByCode(
+                                    $this->getParameter( 'vs_payment.product_category.taxonomy_code' )
+                                );
+        
+        return [
+            'categories'    => $this->get( 'vs_payment.repository.product_category' )->findAll(),
+            'taxonomyId'    => $taxonomy ? $taxonomy->getId() : 0,
+            'translations'  => $translations,
+        ];
+    }
+    
+    protected function prepareEntity( &$entity, &$form, Request $request )
+    {
+        $categories = new ArrayCollection();
+        $pcr        = $this->get( 'vs_payment.repository.product_category' );
+        
+        $formLocale = $request->request->get( 'locale' );
+        $formPost   = $request->request->all( 'product_form' );
+        $formTaxon  = $formPost['category_taxon'];
+        
+        if ( $formLocale ) {
+            $entity->setTranslatableLocale( $formLocale );
+        }
+        
+        if ( $formTaxon ) {
+            foreach ( $formTaxon as $taxonId ) {
+                $category       = $pcr->findOneBy( ['taxon' => $taxonId] );
+                if ( $category ) {
+                    $categories[]   = $category;
+                    $entity->addCategory( $category );
+                }
+            }
+            
+            foreach ( $entity->getCategories() as $cat ) {
+                if ( ! $categories->contains( $cat ) ) {
+                    $entity->removeCategory( $cat );
+                }
+            }
+        }
+    }
+    
+    private function getTranslations()
+    {
+        $translations   = [];
+        $transRepo      = $this->get( 'vs_application.repository.translation' );
+        
+        foreach ( $this->getRepository()->findAll() as $product ) {
+            $translations[$product->getId()] = array_keys( $transRepo->findTranslations( $product ) );
+        }
+        
+        return $translations;
+    }
+}
---
+++ lib/Form/ProductForm.php
@@ -0,0 +1,113 @@
+<?php namespace Vankosoft\PaymentBundle\Form;
+
+use Vankosoft\ApplicationBundle\Form\AbstractForm;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use Vankosoft\PaymentBundle\Model\Product;
+use Vankosoft\PaymentBundle\Model\Interfaces\ProductInterface;
+
+class ProductForm extends AbstractForm
+{
+    /** @var RequestStack */
+    protected $requestStack;
+    
+    /** @var string */
+    protected $categoryClass;
+    
+    public function __construct(
+        RequestStack $requestStack,
+        string $dataClass,
+        string $categoryClass
+    ) {
+        parent::__construct( $dataClass );
+        
+        $this->requestStack         = $requestStack;
+        $this->categoryClass        = $categoryClass;
+    }
+
+    public function buildForm( FormBuilderInterface $builder, array $options ): void
+    {
+        parent::buildForm( $builder, $options );
+        
+        $entity         = $builder->getData();
+        $currentLocale  = $entity->getTranslatableLocale() ?: $this->requestStack->getCurrentRequest()->getLocale();
+        
+        $builder
+            ->add( 'locale', ChoiceType::class, [
+                'label'                 => 'vs_cms.form.locale',
+                'translation_domain'    => 'VSCmsBundle',
+                'choices'               => \array_flip( \Vankosoft\ApplicationBundle\Component\I18N::LanguagesAvailable() ),
+                'data'                  => $currentLocale,
+                'mapped'                => false,
+            ])
+            
+            ->add( 'enabled', CheckboxType::class, [
+                'label'                 => 'vs_payment.form.active',
+                'translation_domain'    => 'VSPaymentBundle',
+            ])  
+            
+            ->add( 'category_taxon', ChoiceType::class, [
+                'label'                 => 'vs_payment.form.categories',
+                'translation_domain'    => 'VSPaymentBundle',
+                'multiple'              => true,
+                'required'              => false,
+                'mapped'                => false,
+                'placeholder'           => 'vs_payment.form.categories_placeholder',
+            ])
+            
+            ->add( 'name', TextType::class, [
+                'label'                 => 'vs_payment.form.name',
+                'translation_domain'    => 'VSPaymentBundle',
+            ])
+            
+            ->add( 'price', NumberType::class, [
+                'label'                 => 'vs_payment.form.price',
+                'required'              => true,
+                'scale'                 => 2,
+                'rounding_mode'         => $options['rounding_mode'],
+                'translation_domain'    => 'VSPaymentBundle',
+            ])
+            
+            ->add( 'currency', EntityType::class, [
+                'label'                 => 'vs_payment.form.currency',
+                'required'              => true,
+                'class'                 => $this->currencyClass,
+                'choice_label'          => 'code',
+                'placeholder'           => 'vs_payment.form.currency_placeholder',
+                'translation_domain'    => 'VSPaymentBundle',
+            ])
+        ;
+    }
+
+    public function configureOptions( OptionsResolver $resolver ): void
+    {
+        parent::configureOptions( $resolver );
+        
+        $resolver
+            ->setDefaults([
+                'csrf_protection'   => false,
+                'rounding_mode'     => \NumberFormatter::ROUND_HALFEVEN,
+            ])
+            
+            ->setDefined([
+                'product',
+            ])
+            
+            ->setAllowedTypes( 'product', ProductInterface::class )
+        ;
+    }
+    
+    public function getName()
+    {
+        return 'vs_payment.product';
+    }
+}
+
