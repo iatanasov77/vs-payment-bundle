f9e2e5745d425af600927853f12cb89db44b8f4c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-03-13 13:37:22 +0000

    Add Currency Field into GatewayConfig.

--- lib/Model/GatewayConfig.php
+++ lib/Model/GatewayConfig.php
@@ -5,6 +5,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
 use Vankosoft\PaymentBundle\Model\Interfaces\PaymentMethodInterface;
+use Vankosoft\PaymentBundle\Model\Interfaces\CurrencyInterface;
 
 class GatewayConfig extends BaseGatewayConfig implements Interfaces\GatewayConfigInterface
 {
