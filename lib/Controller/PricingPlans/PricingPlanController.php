d4ff7dba903aa35f08a567028ef7f08a37ea7f5f
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-08-01 04:50:26 +0000

    Create Controllers, Views and Forms for Pricing Plans Models.

---
+++ lib/Controller/PricingPlans/PricingPlanController.php
@@ -0,0 +1,107 @@
+<?php namespace Vankosoft\PaymentBundle\Controller\PricingPlans;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Collections\ArrayCollection;
+use Vankosoft\ApplicationBundle\Controller\AbstractCrudController;
+
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class PricingPlanController extends AbstractCrudController
+{
+    protected function customData( Request $request, $entity = null ): array
+    {
+        $translations   = $this->classInfo['action'] == 'indexAction' ? $this->getTranslations() : [];
+        
+        $taxonomy   = $this->get( 'vs_application.repository.taxonomy' )->findByCode(
+                                    $this->getParameter( 'vs_payment.product_category.taxonomy_code' )
+                                );
+        
+        $selectedTaxonIds   = [];
+        if ( $this->classInfo['action'] == 'updateAction' ) {
+            foreach ( $entity->getCategories() as $cat ) {
+                $selectedTaxonIds[] = $cat->getTaxon()->getId();
+            }
+        }
+        
+        return [
+            'categories'        => $this->get( 'vs_payment.repository.product_category' )->findAll(),
+            'taxonomyId'        => $taxonomy ? $taxonomy->getId() : 0,
+            'translations'      => $translations,
+            'selectedTaxonIds'  => $selectedTaxonIds,
+        ];
+    }
+    
+    protected function prepareEntity( &$entity, &$form, Request $request )
+    {
+        $categories = new ArrayCollection();
+        $pcr        = $this->get( 'vs_payment.repository.product_category' );
+        
+        $formLocale = $request->request->get( 'locale' );
+        $formPost   = $request->request->all( 'product_form' );
+        $formTaxon  = $formPost['category_taxon'];
+        
+        if ( $formLocale ) {
+            $entity->setTranslatableLocale( $formLocale );
+        }
+        
+        if ( $formTaxon ) {
+            foreach ( $formTaxon as $taxonId ) {
+                $category       = $pcr->findOneBy( ['taxon' => $taxonId] );
+                if ( $category ) {
+                    $categories[]   = $category;
+                    $entity->addCategory( $category );
+                }
+            }
+            
+            foreach ( $entity->getCategories() as $cat ) {
+                if ( ! $categories->contains( $cat ) ) {
+                    $entity->removeCategory( $cat );
+                }
+            }
+        }
+        
+        $pictures    = $request->files->get( 'product_form' );
+        foreach ( $pictures as $pic ) {
+            // echo "<pre>"; var_dump( \reset( $pic )['picture'] ); die;
+            $productPictureFile = \reset( $pic )['picture'];
+            if ( $productPictureFile ) {
+                $this->addProductPicture( $entity, $productPictureFile );
+            }
+        }
+        
+        /** WORKAROUND */
+        foreach ( $entity->getPictures() as $pic ) {
+            if ( empty( $pic->getPath() ) ) {
+                $entity->removePicture( $pic );
+            }
+        }
+    }
+    
+    private function getTranslations()
+    {
+        $translations   = [];
+        $transRepo      = $this->get( 'vs_application.repository.translation' );
+        
+        foreach ( $this->getRepository()->findAll() as $product ) {
+            $translations[$product->getId()] = array_keys( $transRepo->findTranslations( $product ) );
+        }
+        
+        return $translations;
+    }
+    
+    private function addProductPicture( &$entity, File $file ): void
+    {
+        $uploadedFile   = new UploadedFile( $file->getRealPath(), $file->getBasename() );
+        $productPicture = $this->get( 'vs_payment.factory.product_picture' )->createNew();
+        
+        $productPicture->setOriginalName( $file->getClientOriginalName() );
+        $productPicture->setFile( $uploadedFile );
+        
+        $this->get( 'vs_application.app_pictures_uploader' )->upload( $productPicture );
+        $productPicture->setFile( null ); // reset File Because: Serialization of 'Symfony\Component\HttpFoundation\File\UploadedFile' is not allowed
+        
+        $entity->addPicture( $productPicture );
+    }
+}
---
+++ lib/Form/PricingPlanForm.php
@@ -0,0 +1,80 @@
+<?php namespace Vankosoft\PaymentBundle\Form;
+
+use Vankosoft\ApplicationBundle\Form\AbstractForm;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class PricingPlanForm extends AbstractForm
+{
+    /** @var RequestStack */
+    protected $requestStack;
+    
+    /** @var string */
+    protected $categoryClass;
+    
+    public function __construct(
+        RequestStack $requestStack,
+        string $dataClass,
+        string $categoryClass
+        ) {
+            parent::__construct( $dataClass );
+            
+            $this->requestStack         = $requestStack;
+            $this->categoryClass        = $categoryClass;
+    }
+    
+    public function buildForm( FormBuilderInterface $builder, array $options ): void
+    {
+        parent::buildForm( $builder, $options );
+        
+        $entity         = $builder->getData();
+        $currentLocale  = $entity->getTranslatableLocale() ?: $this->requestStack->getCurrentRequest()->getLocale();
+        
+        $builder
+            ->add( 'locale', ChoiceType::class, [
+                'label'                 => 'vs_cms.form.locale',
+                'translation_domain'    => 'VSCmsBundle',
+                'choices'               => \array_flip( \Vankosoft\ApplicationBundle\Component\I18N::LanguagesAvailable() ),
+                'data'                  => $currentLocale,
+                'mapped'                => false,
+            ])
+            
+            ->add( 'enabled', CheckboxType::class, [
+                'label'                 => 'vs_payment.form.active',
+                'translation_domain'    => 'VSPaymentBundle',
+            ])
+            
+            ->add( 'category_taxon', ChoiceType::class, [
+                'label'                 => 'vs_payment.form.categories',
+                'translation_domain'    => 'VSPaymentBundle',
+                'multiple'              => true,
+                'required'              => false,   // Is Required but Used EasyUi
+                'mapped'                => false,
+                'placeholder'           => 'vs_payment.form.categories_placeholder',
+            ])
+            
+            ->add( 'name', TextType::class, [
+                'label'                 => 'vs_payment.form.name',
+                'translation_domain'    => 'VSPaymentBundle',
+            ])
+            
+            ->add( 'description', TextType::class, [
+                'label'                 => 'vs_payment.form.description',
+                'translation_domain'    => 'VSPaymentBundle',
+            ])
+        ;
+    }
+    
+    public function getName()
+    {
+        return 'vs_payment.pricing_plan';
+    }
+}
