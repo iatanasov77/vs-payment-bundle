758223b740ef5621ed4b228feac27dbd68a387da
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-08-01 09:29:15 +0000

    Update PricingPlan Form and View.

--- lib/Form/PricingPlanForm.php
+++ lib/Form/PricingPlanForm.php
@@ -9,9 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 use Vankosoft\PaymentBundle\Model\Interfaces\PricingPlanInterface;
 use Vankosoft\UsersSubscriptionsBundle\Model\PayedServiceSubscriptionPeriod;
