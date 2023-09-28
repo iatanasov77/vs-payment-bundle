706d9e9fa8e97b2a3c3f1363fca3921cbf048a0e
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-09-28 12:53:05 +0000

    Fix PaymentToken Doctrine Mapping.

--- lib/Model/PaymentToken.php
+++ lib/Model/PaymentToken.php
@@ -2,14 +2,18 @@
 
 use Payum\Core\Model\Token;
 use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Model\TimestampableTrait;
 
 class PaymentToken extends Token implements ResourceInterface
 {
+    use TimestampableTrait;
+    
     /** @var int */
     protected $id;
     
     public function getId()
     {
-        return $this->id;
+        //return $this->id;
+        return $this->getHash();
     }
 }
