262290b71da02c9cb90ceb264c2ff3f738d49853
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-10-03 10:18:15 +0000

    Improve Checkout Done Action.

--- lib/Controller/AbstractCheckoutController.php
+++ lib/Controller/AbstractCheckoutController.php
@@ -55,7 +55,7 @@ abstract class AbstractCheckoutController extends AbstractController
         string $paymentClass,
         RepositoryInterface $subscriptionRepository,
         Factory $subscriptionFactory,
-        string $urlRedirectOnDone
+        ?string $urlRedirectOnDone
     ) {
         $this->tokenStorage             = $tokenStorage;
         $this->doctrine                 = $doctrine;
