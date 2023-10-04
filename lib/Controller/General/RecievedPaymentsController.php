942fd23f9e9fb0a8219a4d96168274e0cbf54087
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-10-04 09:23:31 +0000

    Fix RecievedPaymentsController .

--- lib/Controller/General/RecievedPaymentsController.php
+++ lib/Controller/General/RecievedPaymentsController.php
@@ -2,13 +2,25 @@
 
 use Vankosoft\ApplicationBundle\Controller\AbstractCrudController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Intl\Currencies;
 
 class RecievedPaymentsController extends AbstractCrudController
 {
     protected function customData( Request $request, $entity = null ): array
     {
+        $currencies = [];
+        
+        if ( $this->resources ) {
+            foreach ( $this->resources as $currency ) {
+                $currencies[$currency->getCode()]   = [
+                    'symbol'    => Currencies::getSymbol( $currency->getCode() ),
+                    'name'      => Currencies::getName( $currency->getCode() ),
+                ];
+            }
+        }
+        
         return [
-            
+            'intlCurrencies'    => $currencies,
         ];
     }
 }
