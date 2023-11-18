bbf10020245adc1fe16c8b28952809013f3b1231
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-11-18 20:18:43 +0000

    Create Pricing Plan Subscription Show Payments Preview Page.

--- lib/Controller/PaidSubscriptions/PricingPlanSubscriptionsController.php
+++ lib/Controller/PaidSubscriptions/PricingPlanSubscriptionsController.php
@@ -10,11 +10,18 @@ class PricingPlanSubscriptionsController extends AbstractCrudController
     {
         $currencies = [];
         
-        if ( $this->resources ) {
+        if ( $this->classInfo['action'] == 'indexAction' ) {
             foreach ( $this->resources as $subscription ) {
                 $currencies[$subscription->getCurrencyCode()]   = [
                     'symbol'    => Currencies::getSymbol( $subscription->getCurrencyCode() ),
                     'name'      => Currencies::getName( $subscription->getCurrencyCode() ),
+                ];
+            }
+        } elseif ( $this->classInfo['action'] == 'showAction' ) {
+            foreach ( $entity->getOrderItems() as $orderItem ) {
+                $currencies[$subscription->getCurrencyCode()]   = [
+                    'symbol'    => Currencies::getSymbol( $orderItem->getOrder()->getCurrencyCode() ),
+                    'name'      => Currencies::getName( $orderItem->getOrder()->getCurrencyCode() ),
                 ];
             }
         }
