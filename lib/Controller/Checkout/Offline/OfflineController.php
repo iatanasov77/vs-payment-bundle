05afe3b321e635cfa96ae1d87c0fef1268d114c5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-11-19 08:57:00 +0000

    Create Offline BankTransfer Factory

---
+++ lib/Controller/Checkout/Offline/OfflineController.php
@@ -0,0 +1,47 @@
+<?php namespace Vankosoft\PaymentBundle\Controller\Checkout\Offline;
+
+use Vankosoft\PaymentBundle\Controller\AbstractCheckoutController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @NOTE Used for Debug/Test Payum Offline Payments
+ */
+class OfflineController extends AbstractCheckoutController
+{
+    public function prepareAction( Request $request ): Response
+    {
+        $cart   = $this->orderFactory->getShoppingCart();
+        $this->debugGateway( $cart->getPaymentMethod()->getGateway() );
+        
+        $storage = $this->payum->getStorage( $this->paymentClass );
+        $payment = $storage->create();
+        
+        $payment->setOrder( $cart );
+        $payment->setNumber( uniqid() );
+        $payment->setCurrencyCode( $cart->getCurrencyCode() );
+        $payment->setRealAmount( $cart->getTotalAmount() ); // Need this for Real (Human Readable) Amount.
+        $payment->setTotalAmount( $cart->getTotalAmount() );
+        $payment->setDescription( $cart->getDescription() );
+        
+        $user   = $this->tokenStorage->getToken()->getUser();
+        $payment->setClientId( $user ? $user->getId() : 'UNREGISTERED_USER' );
+        $payment->setClientEmail( $user ? $user->getEmail() : 'UNREGISTERED_USER' );
+
+        $storage->update($payment);
+
+        $captureToken   = $this->payum->getTokenFactory()->createCaptureToken(
+            $cart->getPaymentMethod()->getGateway()->getGatewayName(),
+            $payment, 
+            'vs_payment_offline_done' // the route to redirect after capture
+        );
+
+        return $this->redirect( $captureToken->getTargetUrl() );    
+    }
+    
+    public function debugGateway( $gateway )
+    {
+        var_dump( $this->payum->getGateway( $gateway->getGatewayName() ) ); die;
+        var_dump( $gateway ); die;
+    }
+}
