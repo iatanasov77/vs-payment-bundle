a81ec60f52c365be9bf0791ace53d861f5ad4bbf
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-12-02 09:52:59 +0000

    Improve Subscriptions Repository.

--- lib/Repository/PricingPlansSubscriptionsRepository.php
+++ lib/Repository/PricingPlansSubscriptionsRepository.php
@@ -11,7 +11,7 @@ class PricingPlansSubscriptionsRepository extends EntityRepository
         
         $subscriptions  = [];
         foreach ( $collection as $subscription ) {
-            $collection[$subscription->getServiceCode()]    = $subscription;
+            $subscriptions[$subscription->getServiceCode()]    = $subscription;
         }
         
         return $subscriptions;
