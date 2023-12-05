a2ae87622a753907570e52e4d3b345b689236489
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-12-05 17:18:59 +0000

    Create a Stripe Config Controller

--- lib/Form/StripeSubscriptionPlanForm.php
+++ lib/Form/StripeSubscriptionPlanForm.php
@@ -7,7 +7,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class StripeSubscriptionPlanForm
+class StripeSubscriptionPlanForm extends AbstractType
 {
     public function buildForm( FormBuilderInterface $builder, array $options )
     {
