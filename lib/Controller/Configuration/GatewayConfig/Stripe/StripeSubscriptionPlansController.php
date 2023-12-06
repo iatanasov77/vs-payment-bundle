8617bbfabe2486e0829ef96b26c8cd4f4daef1f9
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-12-06 10:29:21 +0000

    Create Some Custom Stripe API Actions.

--- lib/Controller/Configuration/GatewayConfig/Stripe/StripeSubscriptionPlansController.php
+++ lib/Controller/Configuration/GatewayConfig/Stripe/StripeSubscriptionPlansController.php
@@ -28,8 +28,11 @@ class StripeSubscriptionPlansController extends AbstractController
 //         $availablePlans = $this->stripeApi->getPlans();
 //         echo "<pre>"; var_dump( $availablePlans ); die;
         
-        $availableProducts = $this->stripeApi->getProducts();
-        echo "<pre>"; var_dump( $availableProducts ); die;
+//         $availableProducts = $this->stripeApi->getProducts();
+//         echo "<pre>"; var_dump( $availableProducts ); die;
+        
+        $availablePrices = $this->stripeApi->getPrices();
+        echo "<pre>"; var_dump( $availablePrices ); die;
         
         return $this->render( '@VSPayment/Pages/GatewayConfig/Stripe/subscription_plans_index.html.twig', [
             'items' => [],
