787716564db5a259a129552b7236423114a76a43
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-04 08:21:05 +0000

    Fix CouponForm .

--- lib/Form/CouponForm.php
+++ lib/Form/CouponForm.php
@@ -9,6 +9,8 @@ use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 use Vankosoft\PaymentBundle\Model\Interfaces\CouponInterface;
 
