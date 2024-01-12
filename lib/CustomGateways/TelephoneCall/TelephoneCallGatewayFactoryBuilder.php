710937862d01fc3c23c35cd4ff0de058d6af135a
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-12 13:03:17 +0000

    Add ObtainCouponCode Request into TelephoneCall Gateway.

---
+++ lib/CustomGateways/TelephoneCall/TelephoneCallGatewayFactoryBuilder.php
@@ -0,0 +1,33 @@
+<?php namespace Vankosoft\PaymentBundle\CustomGateways\TelephoneCall;
+
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Payum\Core\Bridge\Symfony\Builder\GatewayFactoryBuilder;
+
+class TelephoneCallGatewayFactoryBuilder extends GatewayFactoryBuilder
+{
+    /** @var FormFactoryInterface */
+    private $formFactory;
+    
+    /** @var RequestStack */
+    private $requestStack;
+    
+    public function __construct(
+        $gatewayFactoryClass,
+        FormFactoryInterface $formFactory,
+        RequestStack $requestStack
+    ) {
+        parent::__construct( $gatewayFactoryClass );
+        
+        $this->formFactory  = $formFactory;
+        $this->requestStack = $requestStack;
+    }
+    
+    public function __invoke()
+    {
+        $factory    = call_user_func_array( [$this, 'build'], func_get_args() );
+        $factory->setDependicies( $this->formFactory, $this->requestStack );
+        
+        return $factory;
+    }
+}
