17aef52a33eb554a0f10c147c61bd444e6cf4486
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-19 08:03:33 +0000

    Create a Comment Model to use in Catalog Objects.

--- lib/Model/Interfaces/ContentServiceInterface.php
+++ lib/Model/Interfaces/ContentServiceInterface.php
@@ -3,8 +3,12 @@
 use Sylius\Component\Resource\Model\ResourceInterface;
 use Sylius\Component\Resource\Model\TranslatableInterface;
 use Sylius\Component\Review\Model\ReviewableInterface;
+use Vankosoft\PaymentBundle\Model\Interfaces\CommentableInterface;
 
-interface ContentServiceInterface extends ResourceInterface, TranslatableInterface, ReviewableInterface
+interface ContentServiceInterface extends ResourceInterface, TranslatableInterface, ReviewableInterface, CommentableInterface
 {
-    
+    public function getSlug(): ?string;
+    public function getTitle();
+    public function getDescription();
+    public function getPublished(): ?bool;
 }
--- lib/Model/Traits/ReviewableTrait.php
+++ lib/Model/Traits/ReviewableTrait.php
@@ -2,7 +2,8 @@
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
-use Vankosoft\PaymentBundle\Model\Interfaces\ReviewInterface;
+//use Vankosoft\PaymentBundle\Model\Interfaces\ReviewInterface;
+use Sylius\Component\Review\Model\ReviewInterface;
 
 trait ReviewableTrait
 {
