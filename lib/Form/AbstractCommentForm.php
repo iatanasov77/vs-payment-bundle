79df75572bc0e2d151e1fb4ac064d088491632dd
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-19 13:56:05 +0000

    Add CommentForm For Catalog Comments.

---
+++ lib/Form/AbstractCommentForm.php
@@ -0,0 +1,41 @@
+<?php namespace Vankosoft\PaymentBundle\Form;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+abstract class AbstractCommentForm extends AbstractResourceType
+{
+    public function buildForm( FormBuilderInterface $builder, array $options ): void
+    {
+        $builder
+            ->add( 'parent', HiddenType::class, ['data' => $options['parent_comment']] )
+        
+            ->add( 'comment', TextareaType::class, [
+                'label' => 'vs_payment.form.comment.comment',
+                'attr'  => [
+                    'placeholder' => 'vs_payment.form.comment.comment_placeholder'
+                ],
+            ])
+            
+            ->add( 'btnSubmit', SubmitType::class, [
+                'label' => 'vs_payment.form.comment.submit',
+            ])
+        ;
+    }
+    
+    public function configureOptions( OptionsResolver $resolver ): void
+    {
+        parent::configureOptions( $resolver );
+        
+        $resolver->setDefaults([
+            'translation_domain'    => 'VSPaymentBundle',
+            
+            'parent_comment'        => 0,
+        ]);
+    }
+}
