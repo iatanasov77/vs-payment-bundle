2eff4cb49149731c0a20314d536a963409a0aef1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-02-14 13:09:55 +0000

    Create Promotion Resources.

--- lib/Model/Promotion.php
+++ lib/Model/Promotion.php
@@ -5,5 +5,18 @@ use Vankosoft\PaymentBundle\Model\Interfaces\PromotionInterface;
 
 class Promotion extends BasePromotion implements PromotionInterface
 {
+    /** @var string */
+    protected $locale;
     
+    public function getTranslatableLocale(): ?string
+    {
+        return $this->locale;
+    }
+    
+    public function setTranslatableLocale($locale): self
+    {
+        $this->locale = $locale;
+        
+        return $this;
+    }
 }
