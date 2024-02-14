c0399582662d97afed388a72325bcb0b7ea77b5e
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-02-14 12:57:47 +0000

    Create Promotion Resources.

--- lib/Form/PromotionForm.php
+++ lib/Form/PromotionForm.php
@@ -3,6 +3,8 @@
 use Vankosoft\ApplicationBundle\Form\AbstractForm;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -14,11 +16,33 @@ use Vankosoft\PaymentBundle\Form\Type\PromotionActionCollectionType;
 
 class PromotionForm extends AbstractForm
 {
+    public function __construct(
+        string $dataClass,
+        RequestStack $requestStack,
+        RepositoryInterface $localesRepository
+    ) {
+        parent::__construct( $dataClass );
+        
+        $this->requestStack             = $requestStack;
+        $this->localesRepository        = $localesRepository;
+    }
+    
     public function buildForm( FormBuilderInterface $builder, array $options ): void
     {
         parent::buildForm( $builder, $options );
         
+        $entity         = $builder->getData();
+        $currentLocale  = $entity->getTranslatableLocale() ?: $this->requestStack->getCurrentRequest()->getLocale();
+        
         $builder
+            ->add( 'locale', ChoiceType::class, [
+                'label'                 => 'vs_cms.form.locale',
+                'translation_domain'    => 'VSCmsBundle',
+                'choices'               => \array_flip( $this->fillLocaleChoices() ),
+                'data'                  => $currentLocale,
+                'mapped'                => false,
+            ])
+        
             ->add( 'name', TextType::class, [
                 'label'                 => 'vs_payment.form.name',
                 'translation_domain'    => 'VSPaymentBundle',
