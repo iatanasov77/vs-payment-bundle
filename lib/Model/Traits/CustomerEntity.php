beca0d6bd13c477d765f1973ae308a571d9a754f
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-03-15 08:01:43 +0000

    Improve Model Traiits Attribute ORM Mappings.

--- lib/Model/Traits/CustomerEntity.php
+++ lib/Model/Traits/CustomerEntity.php
@@ -11,6 +11,8 @@ trait CustomerEntity
      * @ORM\ManyToOne(targetEntity="Sylius\Component\Customer\Model\CustomerGroupInterface", inversedBy="customers")
      * @ORM\JoinColumn(name="customer_group_id", referencedColumnName="id", nullable=true)
      */
+    #[ORM\ManyToOne(targetEntity: CustomerGroupInterface::class, inversedBy: "customers")]
+    #[ORM\JoinColumn(name: "customer_group_id", referencedColumnName: "id", nullable: true)]
     protected $group;
     
     public function getGroup(): ?CustomerGroupInterface
