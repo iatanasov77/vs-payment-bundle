10f8f7051d4b1c1a64b44d616d3438cf3623b84d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-05-26 13:43:23 +0000

    Fix Model 'OrderItem'.

--- lib/Model/OrderItem.php
+++ lib/Model/OrderItem.php
@@ -3,6 +3,7 @@
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Vankosoft\PaymentBundle\Model\Interfaces\OrderItemInterface;
+use Vankosoft\PaymentBundle\Model\Interfaces\AdjustmentInterface;
 
 class OrderItem implements OrderItemInterface
 {
