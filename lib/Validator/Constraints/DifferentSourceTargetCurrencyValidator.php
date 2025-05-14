bf84f1634551fe23a82d91be2cf884098192ddae
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-05-14 06:43:13 +0000

    Fix some Deprecations.

--- lib/Validator/Constraints/DifferentSourceTargetCurrencyValidator.php
+++ lib/Validator/Constraints/DifferentSourceTargetCurrencyValidator.php
@@ -8,7 +8,7 @@ use Webmozart\Assert\Assert;
 
 class DifferentSourceTargetCurrencyValidator extends ConstraintValidator
 {
-    public function validate( $value, Constraint $constraint )
+    public function validate( $value, Constraint $constraint ): void
     {
         /** @var DifferentSourceTargetCurrency $constraint */
         Assert::isInstanceOf( $constraint, DifferentSourceTargetCurrency::class );
