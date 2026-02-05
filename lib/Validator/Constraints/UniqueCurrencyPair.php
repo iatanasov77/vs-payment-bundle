071d1decaac1bdcbe28e2f92e643202bfe1bcd9c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-02-05 10:55:12 +0000

    Fix Deprecations in Validator Constraints.

--- lib/Validator/Constraints/UniqueCurrencyPair.php
+++ lib/Validator/Constraints/UniqueCurrencyPair.php
@@ -1,13 +1,26 @@
 <?php namespace Vankosoft\PaymentBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 
 class UniqueCurrencyPair extends Constraint
 {
     /** @var string */
     public $message = 'vs_payment.validation.unique_currency_pair';
     
-    public function getTargets(): string
+    #[HasNamedArguments]
+    public function __construct( ?array $options = null, ?string $message = null, ?array $groups = null, mixed $payload = null )
+    {
+        if ( \is_array( $options ) ) {
+            trigger_deprecation( 'symfony/validator', '7.3', 'Passing an array of options to configure the "%s" constraint is deprecated, use named arguments instead.', static::class );
+        }
+        
+        parent::__construct( $options, $groups, $payload );
+        
+        $this->message = $message ?? $this->message;
+    }
+    
+    public function getTargets(): string|array
     {
         return self::CLASS_CONSTRAINT;
     }
